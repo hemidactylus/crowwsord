41d444ae2114817a671c9d8f8f846b01cfd36a59
hemidactylus <lumidoro@gmail.com>
2018-09-24 19:51:12 +0200

    Made the configuration shape a general object

--- configuration.scala
+++ configuration.scala
@@ -6,50 +6,59 @@
 
 package net.salamandrina.crowwsord
 
-abstract class Configuration[T] (val size: Int, val contents: Seq[T], val pieces: Set[T]) {
+abstract class ConfShape[+T]
+// cells are filled with T, items to insert are U
+abstract class Configuration[T,U] (val shape: ConfShape[Configuration[T,U]], val contents: Seq[T], val pieces: Set[U]) {
   def isCompleted: Boolean
   def isLegal: Boolean
   val objName: String = "GeneralConfig"
   val filler: T
-  def extend(c: T): Configuration[T]
-  def findSolutions: Seq[Configuration[T]] = {
+  def extend(c: U): Configuration[T,U]
+  def findSolutions: Seq[Configuration[T,U]] = {
     if (isCompleted)
       Seq(this)
     else {
       {
         for ( 
-          c: T <- pieces;
-          newC: Configuration[T] = extend(c)
+          c: U <- pieces;
+          newC: Configuration[T,U] = extend(c)
           if newC.isLegal
         ) yield newC
       }.toSeq.flatMap (_.findSolutions)
     }
   }
-  override def toString = (contents ++ Seq.fill(size-contents.length)(filler)).mkString(s"${objName}<","",">")
+  override def toString = s"${objName}<...>"
 }
 
-class CharLine(size: Int, contents: Seq[Char], pieces: Set[Char]) 
-  extends Configuration[Char](size,contents,pieces) {
-  def isCompleted = contents.length==size
+class CharLine(shape: CharLineShape, contents: Seq[Char], pieces: Set[Char]) 
+  extends Configuration[Char,Char](shape,contents,pieces) {
+  def isCompleted = contents.length==shape.length
   val filler: Char = '.'
   override val objName="CharLine"
-  def extend(c: Char): CharLine = new CharLine(size,contents :+ c,pieces)
+  def extend(c: Char): CharLine = new CharLine(shape,contents :+ c,pieces)
   def isLegal = {
     if(contents.length < 2)
       true
     else
       contents.sliding(2) forall ( pair => pair(0) < pair(1) )
   }
+  override def toString = (contents ++ Seq.fill(shape.length-contents.length)(filler)).mkString(s"${objName}<","",">")
 }
+class CharLineShape(val length: Int) extends ConfShape[CharLine]
 
-
-class IntLine(size: Int, contents: Seq[Int], pieces: Set[Int]) 
-  extends Configuration[Int](size,contents,pieces) {
-  def isCompleted = contents.length==size
+class IntLine(shape: IntLineShape, contents: Seq[Int], pieces: Set[Int]) 
+  extends Configuration[Int,Int](shape,contents,pieces) {
+  def isCompleted = contents.length==shape.length
   val filler: Int = -1
   override val objName="IntLine"
-  def extend(c: Int): IntLine = new IntLine(size,contents :+ c,pieces)
+  def extend(c: Int): IntLine = new IntLine(shape,contents :+ c,pieces)
   def isLegal = {
-    contents forall ( _ > 2 )
+    ( contents forall ( _ > 2 ) ) &&
+    (
+      !isCompleted ||
+      ( ( contents :\ 0 ) (_ + _) ) % 2 == 0
+    )
   }
+  override def toString = (contents ++ Seq.fill(shape.length-contents.length)(filler)).mkString(s"${objName}<","",">")
 }
+class IntLineShape(val length: Int) extends ConfShape[IntLine]
--- crowwsord.scala
+++ crowwsord.scala
@@ -4,18 +4,18 @@
   main code
 */
 
-import net.salamandrina.crowwsord.{CharLine,IntLine}
+import net.salamandrina.crowwsord.{CharLine,IntLine,CharLineShape,IntLineShape}
 
 object Crowwsord extends App {
 
   val pieces: Set[Char] = Set('a','b','c','z','d')
-  val confLen: Int = 4
+  val confLen: CharLineShape = new CharLineShape(4)
   val zeroConf: CharLine = new CharLine(confLen,Seq.empty,pieces)
 
   zeroConf.findSolutions foreach ( println(_) )
 
   val ipieces: Set[Int] = Set(1,2,3,4,5)
-  val iconfLen: Int = 3
+  val iconfLen: IntLineShape = new IntLineShape(3)
   val izeroConf: IntLine = new IntLine(iconfLen,Seq.empty,ipieces)
 
   izeroConf.findSolutions foreach ( println(_) )
