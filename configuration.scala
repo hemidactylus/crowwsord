daccf192660b8d0be716a7bababc50e92b5d4f35
hemidactylus <lumidoro@gmail.com>
2018-09-25 07:54:46 +0200

    More generalised and test with steppableIntLine

--- configuration.scala
+++ configuration.scala
@@ -9,34 +9,41 @@ package net.salamandrina.crowwsord
 abstract class ConfShape[+T]
 // cells are filled with T, items to insert are U
 abstract class Configuration[T,U] (val shape: ConfShape[Configuration[T,U]], val contents: Seq[T], val pieces: Set[U]) {
+  type ExtendStep
+  def stepProposals: Seq[ExtendStep]
   def isCompleted: Boolean
-  def canExtendWith(c: U): Boolean
+  def canExtendWith(c: ExtendStep): Boolean
   val objName: String = "GeneralConfig"
   val displayFiller: T
-  def extendWith(c: U): Configuration[T,U]
+  def extendWith(stp: ExtendStep): Configuration[T,U]
   def findSolutions: Seq[Configuration[T,U]] = {
     if (isCompleted)
       Seq(this)
     else {
       {
         for ( 
-          c: U <- pieces;
+          c: ExtendStep <- stepProposals;
           if canExtendWith(c)
         ) yield extendWith(c)
       }.toSeq.flatMap (_.findSolutions)
     }
   }
-  override def toString = s"${objName}<...>"
+  override def toString = s"${objName}< . . . >"
 }
 
-class CharLine(shape: CharLineShape, contents: Seq[Char], pieces: Set[Char]) 
+class CharLine(shape: CharLineShape, contents: Seq[Char], pieces: Set[Char])
   extends Configuration[Char,Char](shape,contents,pieces) {
+  case class CharLineStep(val piece: Char)
+  type ExtendStep = CharLineStep
+  def stepProposals: Seq[ExtendStep] = {
+    ( for (c: Char <- pieces) yield CharLineStep(c)).toSeq
+  }
   def isCompleted = contents.length==shape.length
   val displayFiller: Char = '.'
   override val objName="CharLine"
-  def extendWith(c: Char): CharLine = new CharLine(shape,contents :+ c,pieces)
-  def canExtendWith(c: Char): Boolean = {
-    val newContents: Seq[Char] = contents :+ c
+  def extendWith(stp: ExtendStep): CharLine = new CharLine(shape,contents :+ stp.piece,pieces)
+  def canExtendWith(stp: ExtendStep): Boolean = {
+    val newContents: Seq[Char] = contents :+ stp.piece
     if(newContents.length < 2)
       true
     else
@@ -48,12 +55,17 @@ class CharLineShape(val length: Int) extends ConfShape[CharLine]
 
 class IntLine(shape: IntLineShape, contents: Seq[Int], pieces: Set[Int]) 
   extends Configuration[Int,Int](shape,contents,pieces) {
+  case class IntLineStep(val piece: Int)
+  type ExtendStep = IntLineStep
+  def stepProposals: Seq[ExtendStep] = {
+    ( for (c: Int <- pieces) yield IntLineStep(c)).toSeq
+  }
   def isCompleted = contents.length==shape.length
   val displayFiller: Int = -1
   override val objName="IntLine"
-  def extendWith(c: Int): IntLine = new IntLine(shape,contents :+ c,pieces)
-  def canExtendWith(c: Int): Boolean = {
-    val newContents: Seq[Int] = contents :+ c
+  def extendWith(stp: IntLineStep): IntLine = new IntLine(shape,contents :+ stp.piece,pieces)
+  def canExtendWith(stp: IntLineStep): Boolean = {
+    val newContents: Seq[Int] = contents :+ stp.piece
     ( newContents forall ( _ > 2 ) ) &&
     (
       newContents.length < shape.length ||
@@ -63,3 +75,33 @@ class IntLine(shape: IntLineShape, contents: Seq[Int], pieces: Set[Int])
   override def toString = (contents ++ Seq.fill(shape.length-contents.length)(displayFiller)).mkString(s"${objName}<","",">")
 }
 class IntLineShape(val length: Int) extends ConfShape[IntLine]
+
+class SteppableIntLine(shape: SteppableIntLineShape, contents: Seq[Int]) 
+  extends Configuration[Int,Nothing](shape,contents,Set.empty) {
+  /*
+    contents: list of indices reached in their order
+  */
+  case class SteppableIntLineStep(val pos: Int)
+  type ExtendStep = SteppableIntLineStep
+  def stepProposals: Seq[ExtendStep] = {
+    val lastIndex = if (contents.isEmpty) (shape.length+10) else contents.last
+    val okIndices = (0 until shape.length)
+      .filter( x => ! ( contents contains x ) )
+      .filter(x => Math.abs(lastIndex - x)>1)
+    (for (c <- okIndices) yield new SteppableIntLineStep(c)).toSeq
+  }
+  def isCompleted = contents.size==shape.length
+  val displayFiller: Int = -1
+  override val objName="SteppableIntLine"
+  def extendWith(stp: SteppableIntLineStep): SteppableIntLine = new SteppableIntLine(shape,contents :+ stp.pos)
+  def canExtendWith(stp: SteppableIntLineStep): Boolean = {
+    true
+  }
+  override def toString = {
+    val indMap = (contents zipWithIndex).toMap
+    (0 until shape.length)
+      .map( x => indMap getOrElse(x,displayFiller) )
+      .mkString(s"${objName}<","",">")
+  }
+}
+class SteppableIntLineShape(val length: Int) extends ConfShape[SteppableIntLine]
--- crowwsord.scala
+++ crowwsord.scala
@@ -4,20 +4,27 @@
   main code
 */
 
-import net.salamandrina.crowwsord.{CharLine,IntLine,CharLineShape,IntLineShape}
+import net.salamandrina.crowwsord.{CharLine,CharLineShape}
+import net.salamandrina.crowwsord.{IntLine,IntLineShape}
+import net.salamandrina.crowwsord.{SteppableIntLine,SteppableIntLineShape}
 
 object Crowwsord extends App {
 
+  println("\nCharLine")
   val pieces: Set[Char] = Set('a','b','c','z','d')
   val confLen: CharLineShape = new CharLineShape(4)
   val zeroConf: CharLine = new CharLine(confLen,Seq.empty,pieces)
+  zeroConf.findSolutions foreach ( c => println(s"  ${c}") )
 
-  zeroConf.findSolutions foreach ( println(_) )
-
+  println("\nIntLine")
   val ipieces: Set[Int] = Set(1,2,3,4,5)
   val iconfLen: IntLineShape = new IntLineShape(3)
   val izeroConf: IntLine = new IntLine(iconfLen,Seq.empty,ipieces)
+  izeroConf.findSolutions foreach ( c => println(s"  ${c}") )
 
-  izeroConf.findSolutions foreach ( println(_) )
+  println("\nSteppableIntLine")
+  val sconfLen: SteppableIntLineShape = new SteppableIntLineShape(5)
+  val szeroConf: SteppableIntLine = new SteppableIntLine(sconfLen,Seq.empty)
+  szeroConf.findSolutions foreach ( c => println(s"  ${c}") )
 
 }
