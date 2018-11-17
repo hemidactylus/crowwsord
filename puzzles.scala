dd1e45a8ceb18531b86382f4b2863b3bc52cb2b3
hemidactylus <lumidoro@gmail.com>
2018-11-17 10:55:49 +0100

    Added demos for each of the squarestepper cases

--- puzzles.scala
+++ puzzles.scala
@@ -25,7 +25,29 @@ object PuzzleDemo extends App {
   println("\n[PuzzleDemo] Starting (coinTotal) ...")
   ( coinTotal.findSolutions.zipWithIndex) foreach ( { case (c,i) => println(s"\n    Sol=${i+1}:   ${c}") } )
 
-  // DEMO 3A: squareStepper, quadruplicated
+  // DEMO 3A: squareStepper, monoplicated
+  val squareStepperPuzzleShapeMon: SquareStepperPuzzleShape = new SquareStepperPuzzleShape(
+    (5,5),
+    FillingStrategyEnum.Monoplicate
+  )
+  val squareStepperMon: SquareStepperConfiguration = SquareStepperEnvironment.makeNewConfig(
+    squareStepperPuzzleShapeMon
+  )
+  println("\n[PuzzleDemo] Starting (squareStepper,monoplicated) ...")
+  ( squareStepperMon.findSolutions.zipWithIndex) foreach ( { case (c,i) => println(s"\n    Sol=${i+1}:   ${c}") } )
+
+  // DEMO 3B: squareStepper, duplicated
+  val squareStepperPuzzleShapeDup: SquareStepperPuzzleShape = new SquareStepperPuzzleShape(
+    (5,8),
+    FillingStrategyEnum.Duplicate
+  )
+  val squareStepperDup: SquareStepperConfiguration = SquareStepperEnvironment.makeNewConfig(
+    squareStepperPuzzleShapeDup
+  )
+  println("\n[PuzzleDemo] Starting (squareStepper,duplicated) ...")
+  ( squareStepperDup.findSolutions.zipWithIndex) foreach ( { case (c,i) => println(s"\n    Sol=${i+1}:   ${c}") } )
+
+  // DEMO 3C: squareStepper, quadruplicated
   val squareStepperPuzzleShape: SquareStepperPuzzleShape = new SquareStepperPuzzleShape(
     (10,10),
     FillingStrategyEnum.Quadruplicate
